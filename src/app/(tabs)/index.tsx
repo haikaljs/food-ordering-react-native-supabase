561c3b36ee58345cb07cc26e65ee8d259cd11c1d
haikaljs <haikaljavascript@gmail.com>
2024-10-14 21:58:58 +0800

    Flatlist implementation

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,17 @@
-import { StyleSheet,View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import products from "@assets/data/products";
 import ProductListItem from "@components/ProductListItem";
 
-
-
-
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding:10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1
   },
   title: {
     fontSize: 18,
